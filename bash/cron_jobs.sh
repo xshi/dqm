65b1c7b4ec3cd1aa1fe8b90b0c1265c788859db3
Pixel Dev <pixel_dev@ntucms1.cern.ch>
2013-09-02 00:26:16 +0200

    let the chk_data_integrity control itself

--- bash/cron_jobs.sh
+++ bash/cron_jobs.sh
@@ -4,9 +4,8 @@
 # * * * * * /home/pixel_dev/dqm/bash/cron_jobs.sh
 
 /home/pixel_dev/dqm/python/dqm.py 
+
 /home/pixel_dev/dqm/python/ful.py
 
-if [ $(pgrep chk_data_int | wc -w) -lt 1 ]; then 
-    /home/pixel_dev/dqm/python/chk_data_integrity 
-fi
+/home/pixel_dev/dqm/python/chk_data_integrity 
 
